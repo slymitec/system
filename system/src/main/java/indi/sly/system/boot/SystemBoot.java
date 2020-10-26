962ff182ec36f313c6840dd27845541fbc36920d
slymitec <slymitec@live.com>
2020-10-26 19:44:20 +0800

    Initial commit

--- system/src/main/java/indi/sly/system/boot/SystemBoot.java
+++ system/src/main/java/indi/sly/system/boot/SystemBoot.java
@@ -2,12 +2,19 @@ package indi.sly.system.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({SpringConfiguration.class})
 public class SystemBoot extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        builder.sources(SystemBoot.class);
+        return builder;
+    }
+
     public static void main(String[] args) {
         SpringApplication system = new SpringApplication(null, new Class<?>[]{SystemBoot.class});
         system.run(args);

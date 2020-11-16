bbf3238e45b0847737b79e93eaa66b17429d1d85
slymitec <slymitec@live.com>
2020-11-16 23:48:26 +0800

    Initial commit

--- system/kernel/src/main/java/indi/sly/system/kernel/core/date/prototypes/DateTimeObject.java
+++ system/kernel/src/main/java/indi/sly/system/kernel/core/date/prototypes/DateTimeObject.java
@@ -1,14 +1,36 @@
 package indi.sly.system.kernel.core.date.prototypes;
 
-import java.util.Date;
+import java.time.Clock;
 
+import indi.sly.system.common.exceptions.ConditionPermissionsException;
 import indi.sly.system.kernel.core.prototypes.ACoreObject;
+import indi.sly.system.kernel.processes.ProcessManager;
+import indi.sly.system.kernel.processes.prototypes.ProcessObject;
+import indi.sly.system.kernel.processes.prototypes.ProcessTokenObject;
+import indi.sly.system.kernel.security.prototypes.PrivilegeTypes;
 
 public class DateTimeObject extends ACoreObject {
-    public long getCurrentDateTime() {
-        return System.currentTimeMillis();
+    public DateTimeObject() {
+        this.clock = Clock.systemUTC();
+        this.offset = 0;
     }
 
-    //设置时区
-    //设置时间
+    private Clock clock;
+    private long offset;
+
+    public long getCurrentDateTime() {
+        return this.clock.instant().toEpochMilli() + this.offset;
+    }
+
+    public void setDateTime(long dateTime) {
+        ProcessManager processManager = this.factoryManager.getManager(ProcessManager.class);
+        ProcessObject currentProcess = processManager.getCurrentProcess();
+        ProcessTokenObject currentProcessToken = currentProcess.getToken();
+
+        if (!currentProcessToken.isPrivilegeTypes(PrivilegeTypes.CORE_MODIFY_DATETIME)) {
+            throw new ConditionPermissionsException();
+        }
+
+        this.offset = dateTime - this.clock.instant().toEpochMilli();
+    }
 }

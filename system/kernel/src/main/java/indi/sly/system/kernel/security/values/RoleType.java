cd11b31963e2d017b21c20ff2854f9fffa79b907
Shen Liyang <Slymitec@live.com>
2021-07-20 21:54:41 +0800

    Initial commit

--- system/kernel/src/main/java/indi/sly/system/kernel/security/values/RoleType.java
+++
@@ -1,9 +0,0 @@
-package indi.sly.system.kernel.security.values;
-
-import indi.sly.system.common.supports.UUIDUtil;
-
-import java.util.UUID;
-
-public interface RoleType {
-    UUID NULL = UUIDUtil.getEmpty();
-}

21d81bcebbd5ee4f6c4b5fe36febb7f2b3572a5b
Shen Liyang <Slymitec@live.com>
2021-07-23 23:48:47 +0800

    Initial commit

---
+++ system/kernel/src/main/java/indi/sly/system/kernel/security/instances/prototypes/wrappers/AuditTypeInitializer.java
@@ -0,0 +1,112 @@
+package indi.sly.system.kernel.security.instances.prototypes.wrappers;
+
+import indi.sly.system.common.lang.StatusNotSupportedException;
+import indi.sly.system.common.supports.ObjectUtil;
+import indi.sly.system.common.values.IdentificationDefinition;
+import indi.sly.system.kernel.objects.infotypes.prototypes.wrappers.ATypeInitializer;
+import indi.sly.system.kernel.objects.prototypes.AInfoContentObject;
+import indi.sly.system.kernel.objects.values.DumpDefinition;
+import indi.sly.system.kernel.objects.values.InfoEntity;
+import indi.sly.system.kernel.objects.values.InfoStatusOpenDefinition;
+import indi.sly.system.kernel.objects.values.InfoSummaryDefinition;
+import indi.sly.system.kernel.processes.ProcessManager;
+import indi.sly.system.kernel.processes.instances.prototypes.PortContentObject;
+import indi.sly.system.kernel.processes.instances.values.PortDefinition;
+import indi.sly.system.kernel.processes.prototypes.ProcessObject;
+import indi.sly.system.kernel.processes.prototypes.ProcessTokenObject;
+import indi.sly.system.kernel.processes.values.ProcessTokenLimitType;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+
+import javax.inject.Named;
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+@Named
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class AuditTypeInitializer extends ATypeInitializer {
+    @Override
+    public void install() {
+    }
+
+    @Override
+    public void uninstall() {
+    }
+
+    @Override
+    public UUID getPoolID(UUID id, UUID type) {
+        return this.factoryManager.getKernelSpace().getConfiguration().MEMORY_REPOSITORIES_DATABASEENTITYREPOSITORYOBJECT_ID;
+    }
+
+    @Override
+    public void createProcedure(InfoEntity info) {
+        ProcessManager processManager = this.factoryManager.getManager(ProcessManager.class);
+        ProcessObject process = processManager.getCurrentProcess();
+        ProcessTokenObject processToken = process.getToken();
+
+        PortDefinition port = new PortDefinition();
+
+        port.setProcessID(process.getID());
+        port.setLimit(processToken.getLimits().get(ProcessTokenLimitType.PORT_LENGTH_MAX));
+
+        info.setContent(ObjectUtil.transferToByteArray(port));
+    }
+
+    @Override
+    public void deleteProcedure(InfoEntity info) {
+    }
+
+    @Override
+    public void getProcedure(InfoEntity info) {
+    }
+
+    @Override
+    public void dumpProcedure(InfoEntity info, DumpDefinition dump) {
+    }
+
+    @Override
+    public void openProcedure(InfoEntity info, InfoStatusOpenDefinition statusOpen, long openAttribute,
+                              Object... arguments) {
+    }
+
+    @Override
+    public void closeProcedure(InfoEntity info, InfoStatusOpenDefinition statusOpen) {
+    }
+
+    @Override
+    public void createChildProcedure(InfoEntity info, InfoEntity childInfo) {
+        throw new StatusNotSupportedException();
+    }
+
+    @Override
+    public InfoSummaryDefinition getChildProcedure(InfoEntity info, IdentificationDefinition identification) {
+        throw new StatusNotSupportedException();
+    }
+
+    @Override
+    public Set<InfoSummaryDefinition> queryChildProcedure(InfoEntity info, Predicate<InfoSummaryDefinition> wildcard) {
+        throw new StatusNotSupportedException();
+    }
+
+    @Override
+    public void renameChildProcedure(InfoEntity info, IdentificationDefinition oldIdentification,
+                                     IdentificationDefinition newIdentification) {
+        throw new StatusNotSupportedException();
+    }
+
+    @Override
+    public void deleteChildProcedure(InfoEntity info, IdentificationDefinition identification) {
+        throw new StatusNotSupportedException();
+    }
+
+    @Override
+    public Class<? extends AInfoContentObject> getContentTypeProcedure(InfoEntity info,
+                                                                       InfoStatusOpenDefinition statusOpen) {
+        return PortContentObject.class;
+    }
+
+    @Override
+    public void refreshPropertiesProcedure(InfoEntity info, InfoStatusOpenDefinition statusOpen) {
+    }
+}

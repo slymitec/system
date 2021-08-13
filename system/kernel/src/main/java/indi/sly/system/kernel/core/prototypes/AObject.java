fdb21f4190ae431124d2ad103aa603170d2681c2
Shen Liyang <Slymitec@live.com>
2021-08-13 13:36:29 +0800

    Initial commit

--- system/kernel/src/main/java/indi/sly/system/kernel/core/prototypes/AObject.java
+++ system/kernel/src/main/java/indi/sly/system/kernel/core/prototypes/AObject.java
@@ -1,11 +1,46 @@
 package indi.sly.system.kernel.core.prototypes;
 
+import indi.sly.system.common.lang.StatusAlreadyFinishedException;
+import indi.sly.system.common.supports.UUIDUtil;
+import indi.sly.system.common.supports.ValueUtil;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 
 import javax.inject.Named;
+import java.util.UUID;
 
 @Named
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class AObject extends APrototype {
+    protected UUID handle;
+
+    public final UUID getHandle() {
+        return this.handle;
+    }
+
+    public final UUID cache(long space) {
+        CoreObjectRepositoryObject coreObjectRepository = this.factoryManager.getCoreObjectRepository();
+
+        if (!ValueUtil.isAnyNullOrEmpty(this.handle)) {
+            throw new StatusAlreadyFinishedException();
+        }
+
+        this.handle = UUIDUtil.createRandom();
+
+        coreObjectRepository.addByHandle(space, this.handle, this);
+
+        return this.handle;
+    }
+
+    public final void uncache(long space) {
+        CoreObjectRepositoryObject coreObjectRepository = this.factoryManager.getCoreObjectRepository();
+
+        if (ValueUtil.isAnyNullOrEmpty(this.handle)) {
+            throw new StatusAlreadyFinishedException();
+        }
+
+        coreObjectRepository.deleteByHandle(space, this.handle);
+
+        this.handle = null;
+    }
 }

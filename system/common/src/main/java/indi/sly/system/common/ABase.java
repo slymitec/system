cc8c003df039d7efb8feadb45bf9f6735a533538
Shen Liyang <Slymitec@live.com>
2021-09-06 00:15:20 +0800

    logger节约性能

--- system/common/src/main/java/indi/sly/system/common/ABase.java
+++ system/common/src/main/java/indi/sly/system/common/ABase.java
@@ -1,13 +1,21 @@
 package indi.sly.system.common;
 
+import indi.sly.system.common.supports.ObjectUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public abstract class ABase {
     public ABase() {
         super();
-        this.logger = LogFactory.getLog(this.getClass());
     }
 
-    protected final Log logger;
+    private Log _logger;
+
+    protected synchronized final Log logger() {
+        if (ObjectUtil.isAnyNull(this._logger)) {
+            this._logger = LogFactory.getLog(this.getClass());
+        }
+
+        return this._logger;
+    }
 }

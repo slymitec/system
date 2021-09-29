f42f0a031d0b9d3997c815380ee1dba139fa4115
Shen Liyang <Slymitec@live.com>
2021-09-29 15:06:31 +0800

    优化

--- system/kernel/src/main/java/indi/sly/system/kernel/security/values/AccountAuthorizationTokenDefinition.java
+++ system/kernel/src/main/java/indi/sly/system/kernel/security/values/AccountAuthorizationTokenDefinition.java
@@ -1,12 +1,7 @@
 package indi.sly.system.kernel.security.values;
 
-import indi.sly.system.common.supports.NumberUtil;
-import indi.sly.system.common.supports.UUIDUtil;
 import indi.sly.system.common.values.ADefinition;
 
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
 import java.util.*;
 
 public class AccountAuthorizationTokenDefinition extends ADefinition<AccountAuthorizationTokenDefinition> {
@@ -33,66 +28,5 @@ public class AccountAuthorizationTokenDefinition extends ADefinition<AccountAuth
 
     public Set<UUID> getRoles() {
         return this.roles;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AccountAuthorizationTokenDefinition that = (AccountAuthorizationTokenDefinition) o;
-        return privileges == that.privileges && limits.equals(that.limits) && roles.equals(that.roles);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(privileges, limits, roles);
-    }
-
-    @Override
-    public AccountAuthorizationTokenDefinition deepClone() {
-        AccountAuthorizationTokenDefinition definition = new AccountAuthorizationTokenDefinition();
-
-        definition.privileges = this.privileges;
-        definition.limits.putAll(this.limits);
-        definition.roles.addAll(this.roles);
-
-        return definition;
-    }
-
-    @Override
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        super.readExternal(in);
-
-        this.privileges = NumberUtil.readExternalLong(in);
-
-        int valueInteger;
-
-        valueInteger = NumberUtil.readExternalInteger(in);
-        for (int i = 0; i < valueInteger; i++) {
-            this.limits.put(NumberUtil.readExternalLong(in), NumberUtil.readExternalInteger(in));
-        }
-
-        valueInteger = NumberUtil.readExternalInteger(in);
-        for (int i = 0; i < valueInteger; i++) {
-            this.roles.add(UUIDUtil.readExternal(in));
-        }
-    }
-
-    @Override
-    public void writeExternal(ObjectOutput out) throws IOException {
-        super.writeExternal(out);
-
-        NumberUtil.writeExternalLong(out, this.privileges);
-
-        NumberUtil.writeExternalInteger(out, this.limits.size());
-        for (Map.Entry<Long, Integer> pair : this.limits.entrySet()) {
-            NumberUtil.writeExternalLong(out, pair.getKey());
-            NumberUtil.writeExternalInteger(out, pair.getValue());
-        }
-
-        NumberUtil.writeExternalInteger(out, this.roles.size());
-        for (UUID pair : this.roles) {
-            UUIDUtil.writeExternal(out, pair);
-        }
     }
 }

3c03a119fdc768c0eee941b303c1de4c4f5206d5
Shen Liyang <Slymitec@live.com>
2021-10-08 10:42:57 +0800

    测试

---
+++ system/src/main/java/indi/sly/system/controllers/test/InitController.java
@@ -0,0 +1,101 @@
+package indi.sly.system.controllers.test;
+
+import indi.sly.system.common.supports.StringUtil;
+import indi.sly.system.common.values.IdentificationDefinition;
+import indi.sly.system.controllers.AController;
+import indi.sly.system.kernel.core.enviroment.values.KernelConfigurationDefinition;
+import indi.sly.system.kernel.core.enviroment.values.KernelSpaceDefinition;
+import indi.sly.system.kernel.core.enviroment.values.SpaceType;
+import indi.sly.system.kernel.core.enviroment.values.UserSpaceDefinition;
+import indi.sly.system.kernel.files.instances.prototypes.FileSystemFileContentObject;
+import indi.sly.system.kernel.files.instances.prototypes.FileSystemFolderContentObject;
+import indi.sly.system.kernel.files.instances.values.FileSystemLocationType;
+import indi.sly.system.kernel.objects.ObjectManager;
+import indi.sly.system.kernel.objects.prototypes.InfoObject;
+import indi.sly.system.kernel.objects.values.InfoOpenAttributeType;
+import indi.sly.system.kernel.objects.values.InfoSummaryDefinition;
+import indi.sly.system.kernel.processes.ProcessManager;
+import indi.sly.system.kernel.processes.ThreadManager;
+import indi.sly.system.kernel.processes.prototypes.ProcessObject;
+import indi.sly.system.kernel.security.UserManager;
+import indi.sly.system.kernel.security.prototypes.AccountAuthorizationObject;
+import indi.sly.system.kernel.security.prototypes.AccountObject;
+import indi.sly.system.kernel.security.prototypes.GroupObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+import java.util.*;
+
+@RestController
+public class InitController extends AController {
+    @RequestMapping(value = {"/InitEnv.action"}, method = {RequestMethod.GET})
+    @Transactional
+    public Object initTest(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        this.init();
+
+        UserSpaceDefinition userSpace = new UserSpaceDefinition();
+        KernelSpaceDefinition kernelSpace = this.factoryManager.getKernelSpace();
+        KernelConfigurationDefinition kernelConfiguration = kernelSpace.getConfiguration();
+
+        kernelSpace.getUserSpace().set(userSpace);
+        this.factoryManager.getCoreObjectRepository().setLimit(SpaceType.USER, kernelConfiguration.CORE_ENVIRONMENT_USER_SPACE_CORE_OBJECT_LIMIT);
+
+        ThreadManager threadManager = this.factoryManager.getManager(ThreadManager.class);
+        threadManager.create(kernelConfiguration.PROCESSES_PROTOTYPE_SYSTEM_ID);
+
+        Map<String, Object> result = new HashMap<>();
+
+        //--Start--
+
+        ObjectManager objectManager = this.factoryManager.getManager(ObjectManager.class);
+
+        InfoObject parentInfo = objectManager.get(List.of(new IdentificationDefinition("Files")));
+
+        Set<InfoSummaryDefinition> infoSummaries = parentInfo.queryChild(infoSummary -> "Volume".equals(infoSummary.getName()));
+        if (infoSummaries.isEmpty()) {
+            InfoObject childInfo = parentInfo.createChildAndOpen(kernelConfiguration.FILES_TYPES_INSTANCE_FOLDER_ID,
+                    new IdentificationDefinition("Volume"), InfoOpenAttributeType.OPEN_EXCLUSIVE);
+            FileSystemFolderContentObject folderContent = (FileSystemFolderContentObject) childInfo.getContent();
+            folderContent.setType(FileSystemLocationType.MAPPING);
+            folderContent.setValue(StringUtil.writeToBytes("C:/Users/Sly/Desktop/SlySystem/Volume"));
+            folderContent.close();
+        }
+
+        parentInfo = objectManager.get(List.of(new IdentificationDefinition("Files"), new IdentificationDefinition("Volume")));
+        infoSummaries = parentInfo.queryChild(infoSummary -> "Test.bin".equals(infoSummary.getName()));
+        if (infoSummaries.isEmpty()) {
+            InfoObject childInfo = parentInfo.createChildAndOpen(kernelConfiguration.FILES_TYPES_INSTANCE_FILE_ID,
+                    new IdentificationDefinition("Test.bin"), InfoOpenAttributeType.OPEN_EXCLUSIVE);
+            FileSystemFileContentObject fileContent = (FileSystemFileContentObject) childInfo.getContent();
+            fileContent.write(StringUtil.writeToBytes("{\"id\":\"f912d8f2-37ed-4c11-88e0-cb4a6e7eb147\",\"supportedSession\":2,\"name\":\"测试程序\",\"serverURL\":\"http://1.2.3.4\",\"configurations\":{\"配置1\":\"数值1\",\"配置2\":\"数值2\"}}"));
+            fileContent.close();
+        }
+
+        UserManager userManager = this.factoryManager.getManager(UserManager.class);
+        Set<GroupObject> groups = new HashSet<>();
+        groups.add(userManager.getGroup("Administrators"));
+        groups.add(userManager.getGroup("Users"));
+        try {
+            AccountObject account = userManager.createAccount("Sly", "s34l510y24");
+            account.setGroups(groups);
+        } catch (Exception ignored) {
+        }
+        AccountAuthorizationObject accountAuthorization = userManager.authorize("Sly", null);
+
+        ProcessManager processManager = this.factoryManager.getManager(ProcessManager.class);
+
+        InfoObject execInfo = objectManager.get(List.of(new IdentificationDefinition("Files"),
+                new IdentificationDefinition("Volume"), new IdentificationDefinition("Test.bin")));
+        UUID handle = execInfo.open(InfoOpenAttributeType.OPEN_EXCLUSIVE);
+        ProcessObject process = processManager.create(accountAuthorization, handle, null, null);
+
+        result.put("ProcessID", process.getID());
+
+        return result;
+    }
+}
--- system/src/main/java/indi/sly/system/controllers/test/TestController.java
+++ system/src/main/java/indi/sly/system/controllers/test/TestController.java
@@ -1,14 +1,12 @@
 package indi.sly.system.controllers.test;
 
-import indi.sly.system.common.supports.ObjectUtil;
 import indi.sly.system.controllers.AController;
 import indi.sly.system.kernel.core.enviroment.values.KernelConfigurationDefinition;
 import indi.sly.system.kernel.core.enviroment.values.KernelSpaceDefinition;
 import indi.sly.system.kernel.core.enviroment.values.SpaceType;
 import indi.sly.system.kernel.core.enviroment.values.UserSpaceDefinition;
-import indi.sly.system.services.job.JobService;
-import indi.sly.system.services.job.prototypes.UserContentObject;
-import indi.sly.system.services.job.prototypes.UserContextObject;
+import indi.sly.system.kernel.processes.ProcessManager;
+import indi.sly.system.kernel.processes.ThreadManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,65 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class TestController extends AController {
-    public TestController() {
-    }
-
-    private UserSpaceDefinition userSpace;
-
-    @RequestMapping(value = {"/TestWS.action"}, method = {RequestMethod.GET})
+    @RequestMapping(value = {"/Test.action"}, method = {RequestMethod.GET})
+    @Transactional
     public Object Test(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         this.init();
 
-        if (ObjectUtil.isAnyNull(this.factoryManager)) {
-            return "系统未启动。";
-        }
-
+        UserSpaceDefinition userSpace = new UserSpaceDefinition();
         KernelSpaceDefinition kernelSpace = this.factoryManager.getKernelSpace();
         KernelConfigurationDefinition kernelConfiguration = kernelSpace.getConfiguration();
 
-//        UserContextRequestRawDefinition userContextRequestRaw = new UserContextRequestRawDefinition();
-//
-//        userContextRequestRaw.getProcessID().setProcessID(kernelConfiguration.PROCESSES_PROTOTYPE_SYSTEM_ID);
-//
-//        UserContextRequestContentRawDefinition userContextRequestContentRaw = userContextRequestRaw.getContent();
-//
-//        userContextRequestContentRaw.setTask("Manager");
-//        userContextRequestContentRaw.setMethod("processGetCurrent");
-//
-//        if (2 > 1) {
-//            return ObjectUtil.transferToString(userContextRequestRaw);
-//        }
-
-        String requestText = """
-                {"processID":{"processID":"9c337a14-f7f4-432e-ab4a-c8e95943d31f"},"content":{"task":"Manager","method":"processGetCurrent","request":{}}}
-                """;
-
-        this.userSpace = new UserSpaceDefinition();
-
-
-        kernelSpace.getUserSpace().set(this.userSpace);
+        kernelSpace.getUserSpace().set(userSpace);
         this.factoryManager.getCoreObjectRepository().setLimit(SpaceType.USER, kernelConfiguration.CORE_ENVIRONMENT_USER_SPACE_CORE_OBJECT_LIMIT);
 
+        ThreadManager threadManager = this.factoryManager.getManager(ThreadManager.class);
+        threadManager.create(kernelConfiguration.PROCESSES_PROTOTYPE_SYSTEM_ID);
 
-        JobService jobService = this.factoryManager.getService(JobService.class);
+        Map<String, Object> result = new HashMap<>();
 
-        UserContextObject userContext = jobService.createUserContext(requestText);
+        //--Start--
 
-        UserContentObject userContextContent = userContext.getContent();
+        ProcessManager processManager = this.factoryManager.getManager(ProcessManager.class);
 
-        userContextContent.run();
 
-        Map<String, Object> ret = new HashMap<>();
 
-        ret.put("response", userContext.getResponse());
 
-        return ret;
+
+        return result;
     }
-
-
 }

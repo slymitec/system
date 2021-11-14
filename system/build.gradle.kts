ac13e3ec428bfa84ad164c0a543d2d1426a1af3e
Shen Liyang <Slymitec@live.com>
2021-11-14 11:11:56 +0800

    Initial commit

--- system/build.gradle.kts
+++ system/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version ("2.5.5")
+    id("org.springframework.boot") version ("2.5.6")
     id("io.spring.dependency-management") version ("1.0.11.RELEASE")
     kotlin("jvm") version ("1.5.31")
     kotlin("plugin.spring") version ("1.5.31")

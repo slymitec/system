5e1efd6201dbff0a60abbd43a9be0107ec4a416f
Shen Liyang <Slymitec@live.com>
2023-01-10 23:16:16 +0800

    Initial commit

--- system/src/main/java/indi/sly/system/boot/SystemBoot.java
+++ system/src/main/java/indi/sly/system/boot/SystemBoot.java
@@ -1,11 +1,9 @@
 package indi.sly.system.boot;
 
-import indi.sly.system.kernel.core.FactoryManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -19,15 +17,6 @@ public class SystemBoot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication system = new SpringApplication(null, new Class<?>[]{SystemBoot.class});
-        ConfigurableApplicationContext applicationContext = system.run(args);
-
-        try {
-            applicationContext.getBean(FactoryManager.class);
-        }catch (Exception e)
-        {
-            System.out.println("!!! e");
-            System.out.println(e.getMessage());
-        }
-
+        system.run(args);
     }
 }

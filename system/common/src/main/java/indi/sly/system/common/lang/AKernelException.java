38592828fdc18f8329b469bddf76e21df36bd2e1
Shen Liyang <Slymitec@live.com>
2023-07-13 23:38:46 +0800

    优化

--- system/common/src/main/java/indi/sly/system/common/lang/AKernelException.java
+++ system/common/src/main/java/indi/sly/system/common/lang/AKernelException.java
@@ -5,8 +5,6 @@ import indi.sly.system.common.supports.StringUtil;
 public abstract class AKernelException extends RuntimeException {
     private static final long serialVersionUID = 3353990103481301713L;
 
-    protected final Class<? extends AKernelException> type;
-
     public AKernelException(Class<? extends AKernelException> type, String message) {
         super(type.getName() + (message != null ? " (" + message + ")" : StringUtil.EMPTY));
         this.type = type;
@@ -15,6 +13,8 @@ public abstract class AKernelException extends RuntimeException {
     public AKernelException(Class<? extends AKernelException> type) {
         this(type, null);
     }
+
+    protected final Class<? extends AKernelException> type;
 
     public Class<? extends AKernelException> getType() {
         return this.type;

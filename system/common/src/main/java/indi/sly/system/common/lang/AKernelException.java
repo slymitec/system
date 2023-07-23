46304a0e5641d3f8c674f1949341ba9ccdabc802
Shen Liyang <Slymitec@live.com>
2023-07-23 10:35:04 +0800

    删除Exception中的message

--- system/common/src/main/java/indi/sly/system/common/lang/AKernelException.java
+++ system/common/src/main/java/indi/sly/system/common/lang/AKernelException.java
@@ -1,17 +1,11 @@
 package indi.sly.system.common.lang;
 
-import indi.sly.system.common.supports.StringUtil;
-
 public abstract class AKernelException extends RuntimeException {
     private static final long serialVersionUID = 3353990103481301713L;
 
-    public AKernelException(Class<? extends AKernelException> type, String message) {
-        super(type.getName() + (message != null ? " (" + message + ")" : StringUtil.EMPTY));
-        this.type = type;
-    }
-
     public AKernelException(Class<? extends AKernelException> type) {
-        this(type, null);
+        super(type.getName());
+        this.type = type;
     }
 
     protected final Class<? extends AKernelException> type;

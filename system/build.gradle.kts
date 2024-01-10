a9df7f99514e248ace73874766dc1d66d68d1b64
Shen Liyang <Slymitec@live.com>
2024-01-10 20:56:40 +0800

    JDK 21

--- system/build.gradle.kts
+++ system/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     java
-    id("org.springframework.boot") version ("3.1.4")
-    id("io.spring.dependency-management") version ("1.1.3")
-    kotlin("jvm") version ("1.8.22")
-    kotlin("plugin.spring") version ("1.8.22")
+    id("org.springframework.boot") version ("3.2.1")
+    id("io.spring.dependency-management") version ("1.1.4")
+    kotlin("jvm") version ("1.9.21")
+    kotlin("plugin.spring") version ("1.9.21")
 }
 
 allprojects {

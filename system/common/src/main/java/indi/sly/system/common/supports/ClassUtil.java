070edacef71296f6a20b7b9125693a38d2b31a05
Shen Liyang <Slymitec@live.com>
2025-04-13 17:56:56 +0800

    修复

--- system/common/src/main/java/indi/sly/system/common/supports/ClassUtil.java
+++ system/common/src/main/java/indi/sly/system/common/supports/ClassUtil.java
@@ -1,12 +1,18 @@
 package indi.sly.system.common.supports;
 
+import indi.sly.system.common.ABase;
 import indi.sly.system.common.lang.ConditionParametersException;
+import org.apache.commons.lang3.ClassUtils;
 
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 
-public abstract class ClassUtil extends org.apache.commons.lang3.ClassUtils {
+public abstract class ClassUtil {
+    public static String getAbbreviatedName(Class<? extends ABase> clazz, int lengthHint) {
+        return ClassUtils.getAbbreviatedName(clazz, lengthHint);
+    }
+
     public static boolean isThisOrSuperContain(Class<?> child, Class<?> parent) {
         if (ObjectUtil.isAnyNull(child, parent)) {
             throw new ConditionParametersException();

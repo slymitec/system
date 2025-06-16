9740598051b79ff5ab2731c6b4af2f1c1bf5336a
Shen Liyang <Slymitec@live.com>
2025-06-16 21:53:14 +0800

    升级Gradle

--- system/build.gradle.kts
+++ system/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version ("3.5.0")
     id("io.spring.dependency-management") version ("1.1.7")
-    kotlin("jvm") version ("1.9.25")
-    kotlin("plugin.spring") version ("1.9.25")
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
 }
 
 allprojects {

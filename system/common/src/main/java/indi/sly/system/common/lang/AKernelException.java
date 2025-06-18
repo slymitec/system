ade90d7697aa2f683b06bf57f896e58492701346
Shen Liyang <Slymitec@live.com>
2025-06-18 20:11:06 +0800

    优化

--- system/common/src/main/java/indi/sly/system/common/lang/AKernelException.java
+++ system/common/src/main/java/indi/sly/system/common/lang/AKernelException.java
@@ -1,6 +1,9 @@
 package indi.sly.system.common.lang;
 
+import java.io.Serial;
+
 public abstract class AKernelException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 3353990103481301713L;
 
     public AKernelException(Class<? extends AKernelException> type) {

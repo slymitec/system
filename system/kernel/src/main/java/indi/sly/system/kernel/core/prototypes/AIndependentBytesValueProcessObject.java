c93f6aaf103fe163593111f795dcb3ab969e0e09
Shen Liyang <Slymitec@live.com>
2026-04-05 11:55:05 +0800

    优化依赖

--- system/kernel/src/main/java/indi/sly/system/kernel/core/prototypes/AIndependentBytesValueProcessObject.java
+++ system/kernel/src/main/java/indi/sly/system/kernel/core/prototypes/AIndependentBytesValueProcessObject.java
@@ -8,18 +8,5 @@ import jakarta.inject.Named;
 
 @Named
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public abstract class AIndependentBytesValueProcessObject<T> extends AIndependentProcessObject<byte[]> {
-    protected T value;
-
-    protected void read(byte[] source) {
-        if (ObjectUtil.isAnyNull(source)) {
-            this.value = null;
-        } else {
-            this.value = ObjectUtil.transferFromByteArray(source);
-        }
-    }
-
-    protected byte[] write() {
-        return ObjectUtil.isAnyNull(this.value) ? null : ObjectUtil.transferToByteArray(this.value);
-    }
+public abstract class AIndependentBytesValueProcessObject<T> extends ABytesValueProcessObject<T, AObject> {
 }
--- system/kernel/src/main/java/indi/sly/system/kernel/core/prototypes/AIndependentValueProcessObject.java
+++ system/kernel/src/main/java/indi/sly/system/kernel/core/prototypes/AIndependentValueProcessObject.java
@@ -7,14 +7,5 @@ import jakarta.inject.Named;
 
 @Named
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-public abstract class AIndependentValueProcessObject<T> extends AIndependentProcessObject<T> {
-    protected T value;
-
-    protected void read(T source) {
-        this.value = source;
-    }
-
-    protected T write() {
-        return this.value;
-    }
+public abstract class AIndependentValueProcessObject<T> extends AValueProcessObject<T, AObject> {
 }

e55376554cb298ea1f873e52570fcb8b8b1a671f
Shen Liyang <Slymitec@live.com>
2026-04-06 22:14:41 +0800

    修复

--- subsystem/build.gradle.kts
+++ subsystem/build.gradle.kts
@@ -32,6 +32,9 @@ subprojects {
 }
 
 dependencies {
+    implementation(project(":common"))
+    implementation(project(":periphery"))
+
     implementation("jakarta.inject:jakarta.inject-api:2.0.1")
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")

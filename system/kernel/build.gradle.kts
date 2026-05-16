918d96a3bf97c9b3b085abbcb9b904cf2cef9414
Shen Liyang <Slymitec@live.com>
2026-05-16 17:10:58 +0800

    Redisson

--- system/kernel/build.gradle.kts
+++ system/kernel/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("com.redis.om:redis-om-spring:2.0.4")
+    implementation("org.redisson:redisson-spring-boot-starter:4.4.0")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-redis-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ system/kernel/src/main/java/indi/sly/system/kernel/memory/repositories/prototypes/CommunicationRepositoryObject.java
@@ -0,0 +1,55 @@
+package indi.sly.system.kernel.memory.repositories.prototypes;
+
+import com.github.f4b6a3.ulid.Ulid;
+import indi.sly.system.common.lang.ConditionParametersException;
+import indi.sly.system.common.supports.ValueUtil;
+import indi.sly.system.kernel.core.prototypes.AObject;
+import jakarta.annotation.Resource;
+import jakarta.inject.Named;
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+
+import java.util.UUID;
+
+@Named
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class CommunicationRepositoryObject extends AObject {
+    @Resource
+    private RedissonClient redissonClient;
+
+    public RLock getLock(UUID id) {
+        if (ValueUtil.isAnyNullOrEmpty(id)) {
+            throw new ConditionParametersException();
+        }
+
+        return this.redissonClient.getLock(Ulid.from(id).toString());
+    }
+
+    public RReadWriteLock getReadWriteLock(UUID id) {
+        if (ValueUtil.isAnyNullOrEmpty(id)) {
+            throw new ConditionParametersException();
+        }
+
+        return this.redissonClient.getReadWriteLock(Ulid.from(id).toString());
+    }
+
+    public RAtomicLong getAtomicLong(UUID id, String parameter) {
+        if (ValueUtil.isAnyNullOrEmpty(id)) {
+            throw new ConditionParametersException();
+        }
+
+        String key;
+        if (ValueUtil.isAnyNullOrEmpty(parameter)) {
+            key = Ulid.from(id).toString();
+        } else {
+            key = Ulid.from(id) + parameter;
+        }
+
+        return this.redissonClient.getAtomicLong(key);
+    }
+
+}

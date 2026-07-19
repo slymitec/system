9054ceb5ae3a31d85d0d9e63aa678d9100ca85c4
Shen Liyang <Slymitec@live.com>
2026-07-19 12:34:51 +0800

    更新

--- subsystem/common/build.gradle.kts
+++ subsystem/common/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api("jakarta.inject:jakarta.inject-api:2.0.1")
     implementation("org.apache.commons:commons-collections4:4.4")
     implementation("org.apache.commons:commons-lang3")
-    api("org.apache.fory:fory-core:1.1.0")
+    api("org.apache.fory:fory-core:1.3.0")
     api("com.github.f4b6a3:uuid-creator:6.1.1")
     api("org.springframework.boot:spring-boot-starter")
     api("tools.jackson.core:jackson-core")

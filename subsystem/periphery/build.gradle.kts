74c2396fe4323f47afa4f6a03bb98452c46fac5b
Shen Liyang <Slymitec@live.com>
2026-07-19 12:35:51 +0800

    更新

--- subsystem/periphery/build.gradle.kts
+++ subsystem/periphery/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     api("org.springframework.boot:spring-boot-starter-data-redis")
     api("org.springframework.boot:spring-boot-starter-restclient")
-    api("org.redisson:redisson-spring-boot-starter:4.6.0")
+    api("org.redisson:redisson-spring-boot-starter:4.6.1")
     api("org.java-websocket:Java-WebSocket:1.6.0")
     api("tools.jackson.core:jackson-core")
     api("tools.jackson.core:jackson-databind")
--- system/kernel/build.gradle.kts
+++ system/kernel/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     api("org.springframework.boot:spring-boot-starter-data-redis")
     api("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("org.redisson:redisson-spring-boot-starter:4.6.0")
+    implementation("org.redisson:redisson-spring-boot-starter:4.6.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
